20ae2a063d60cb266ff3f3bb32b9145599c0a97a
Alexis <alextron0102@gmail.com>
2021-07-16 21:00:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function compute()
     var principal = document.getElementById("principal").value;
     if(principal<1){
         alert("Enter a positive number");
+        document.getElementById("principal").focus();
         return false;
     }
     var rate = document.getElementById("rate").value;
